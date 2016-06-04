5a4c53fb9dc73606f11c18c0512d7c475ed45f72
Millan Philipose <millan.philipose@gmail.com>
2016-06-03 17:30:19 -0700

    create starter page

--- suggestionsapp/views.py
+++ suggestionsapp/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from suggestionsapp import models
 
 
 class SuggestionsListView(ListView):
     model = models.Suggestion
     template_name = 'suggestionsapp/home.html'
+
+
+class SuggestionDetailView(DetailView):
+    model = models.Suggestion
+    template_name = 'suggestionsapp/suggestion.html'
