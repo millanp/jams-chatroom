cbef92e47f7333c1faff86b77d5e1b6a7c5ea007
Millan Philipose <millan.philipose@gmail.com>
2016-06-06 19:49:54 -0700

    add static_root

--- jamsgreatagain/settings.py
+++ jamsgreatagain/settings.py
@@ -126,6 +126,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 # STATICFILES_STORAGE = 'pipeline.storage.PipelineCachedStorage'
 # STATICFILES_FINDERS = (
