fa02c0e3b13a73bb6e8daf4c72e7e90d1958181e
Millan Philipose <millan.philipose@gmail.com>
2016-06-06 19:36:02 -0700

    add static_root

--- jamsgreatagain/settings.py
+++ jamsgreatagain/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'pipeline.storage.PipelineCachedStorage'
 STATICFILES_FINDERS = (
