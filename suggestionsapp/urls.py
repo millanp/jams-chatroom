9281e3a75438c5a81ba3313bf446200b27f252cf
Millan Philipose <millan.philipose@gmail.com>
2016-06-06 19:27:31 -0700

    change urls

--- suggestionsapp/urls.py
+++ suggestionsapp/urls.py
@@ -3,5 +3,5 @@ from django.contrib import admin
 from suggestionsapp import views
 
 urlpatterns = [
-    url(r'^', views.SuggestionsListView.as_view(), name='suggestions-list'),
+    url(r'^$', views.SuggestionsListView.as_view(), name='suggestions-list'),
 ]
