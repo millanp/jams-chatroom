82af00fcf96f3f6b6c11c7ca53f131d9ce071363
Millan Philipose <millan.philipose@gmail.com>
2016-06-06 19:43:13 -0700

    add static_root

--- jamsgreatagain/settings.py
+++ jamsgreatagain/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # A package to speed up static file serving (CSS, JS, images, etc.)
-    'pipeline',
+    # 'pipeline',
     # The thing we are creating
     'suggestionsapp',
 ]
